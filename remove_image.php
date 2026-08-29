473d7e734395bd7c9ca2d83dfe67e0c461d5a2f3
Alex <klepalovalex@gmail.com>
2026-08-29 19:31:32 +0500

    Delete remove_image.php

--- remove_image.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-require __DIR__ . '/bootstrap.php';
-require_admin();
-$idx=(int)($_POST['index']??-1);
-if($idx<0 || $idx>3) json_response(['ok'=>false,'error'=>'Invalid slide'],400);
-$dir=dirname(__DIR__).'/uploads/culture';
-foreach(glob($dir.'/slide'.$idx.'.*') ?: [] as $old){ @unlink($old); }
-json_response(['ok'=>true]);
